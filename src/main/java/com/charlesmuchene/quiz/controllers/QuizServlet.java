027501c5f8404045885e3fd338e50d2eb96545be
Charles Muchene <charlesmuchene@gmail.com>
2019-01-31 21:20:53 -0600

    Adds servlet dependency and quiz servlet

--- src/main/java/com/charlesmuchene/quiz/controllers/QuizServlet.java
+++ src/main/java/com/charlesmuchene/quiz/controllers/QuizServlet.java
@@ -1,4 +1,22 @@
 package com.charlesmuchene.quiz.controllers;
 
-public class QuizServlet {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "Quiz", urlPatterns = "/quiz")
+public class QuizServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
 }
