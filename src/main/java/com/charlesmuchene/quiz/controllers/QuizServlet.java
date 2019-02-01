6b512fe948a2fcec6e894f12116cc17248d89ad4
Charles Muchene <charlesmuchene@gmail.com>
2019-01-31 22:08:07 -0600

    Adds jsp web framework

--- src/main/java/com/charlesmuchene/quiz/controllers/QuizServlet.java
+++ src/main/java/com/charlesmuchene/quiz/controllers/QuizServlet.java
@@ -1,5 +1,8 @@
 package com.charlesmuchene.quiz.controllers;
 
+import com.charlesmuchene.quiz.data.InMemoryData;
+import com.charlesmuchene.quiz.models.Quiz;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,6 +17,8 @@ import java.io.IOException;
  */
 @WebServlet(name = "Quiz", urlPatterns = "/quiz")
 public class QuizServlet extends HttpServlet {
+
+    private final Quiz quiz = new Quiz(new InMemoryData());
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
---
+++ web/index.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" %>
+<html>
+<head>
+    <title>Number Quiz</title>
+</head>
+<body>
+<h1>Awesome number tests.</h1>
+<button>Begin</button>
+</body>
+</html>
